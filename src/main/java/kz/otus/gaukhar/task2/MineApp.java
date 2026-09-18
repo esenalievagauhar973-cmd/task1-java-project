f31d0754153f9789acbfd161d876fbcd498776a3
esenalievagauhar973-cmd <esenalievagauhar973@gmail.com>
2026-09-18 20:50:26 +0500

    Initial commit

---
+++ src/main/java/kz/otus/gaukhar/task2/MineApp.java
@@ -0,0 +1,14 @@
+package kz.otus.gaukhar.task2;
+
+public class MineApp {
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+
+    public static void main(String[] args) {
+        greetings();
+    }
+}
